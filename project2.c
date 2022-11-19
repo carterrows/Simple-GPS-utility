fa1ae78f75b50e067f9c34de5c24a93019e4aa9f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 15:12:48 -0500

    created project2.c

---
+++ project2.c
@@ -0,0 +1,13 @@
+// Mini Project 2
+// Made by Carter Rows
+// November 2022
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("\nWelcome to my GPS Utility!\n\n");
+    
+    return 0;
+}
